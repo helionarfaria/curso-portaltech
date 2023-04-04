0371b9a58daf2dd2aa37a7b8b469cbba2f700bd2
helionarfaria <helionar02faria@hotmail.com>
2023-04-03 21:55:39 -0300

    Envio da aula 10 - HTML 01

---
+++ calculadora_com_sair.py
@@ -0,0 +1,32 @@
+def calc(num1, num2, tipo):
+
+    tipo = "x"
+
+    while (tipo != 0):
+      
+      if tipo == 0:
+        print("Saindo...")
+        break
+      
+      elif tipo == 1:
+        float(num1 + num2)
+
+      elif tipo == 2:
+        float(num1 - num2)
+
+      elif tipo == 3:
+        float(num1 * num2)
+
+      elif tipo == 4:
+        float(num1 / num2)
+
+      else:
+        print("Tipo de operação selecionado inválido, tente novamente selecionando 1, 2, 3 ou 4")
+
+    return operacao
+
+tipo = int(input("Para números fracionados, utilizar . (ponto) ao invés de vírgula.\nDigite o tipo de operação, sendo:\n1 para adição;\n2 para subtração;\n3 para multiplicação\n4 para divisão;\n0 para sair: "))
+num1 = float(input("Digite um número: "))
+num2 = float(input("Digite outro número: "))
+operacao = calc(num1, num2, tipo)
+print(operacao)
